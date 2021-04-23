2240e6c3b9569249046d50ae466e2d49bada0a0c
saalkred <82131401+saalkred@users.noreply.github.com>
2021-04-22 22:53:09 -0700

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -15,6 +15,7 @@ public class GUI implements ActionListener {
 	private static JLabel passwordLabel;
 	private static JPasswordField passwordText;
 	private static JButton LoginButton;
+	private static JButton CreateAccount;
 	private static JLabel LoginSuccessful;
 	
 
@@ -22,34 +23,42 @@ public class GUI implements ActionListener {
 		// TODO Auto-generated method stub
 		JPanel panel= new JPanel();
 		JFrame frame= new JFrame();
-		frame.setSize(400,400);
+		frame.setSize(1200,850);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(panel);
 
-
+		JLabel title = new JLabel(" Shop ");
+		title.setBounds(500, 100, 100, 100);
+		panel.add(title);
+		
 		panel.setLayout(null);
 		//
 		
 		useNameLabel = new JLabel(" User Name ");
-		useNameLabel.setBounds(150, 90, 100, 25);
+		useNameLabel.setBounds(360, 220, 100, 25);
 		panel.add(useNameLabel);
 		
 		userNameText = new JTextField(20);
-		userNameText.setBounds(115, 120, 150, 25);
+		userNameText.setBounds(450, 220, 150, 25);
 		panel.add(userNameText);
 		//
 		
 		passwordLabel = new JLabel(" Password");
-		passwordLabel.setBounds(150, 170, 100, 25);
+		passwordLabel.setBounds(360, 320, 100, 25);
 		panel.add(passwordLabel);
 		
 		passwordText = new JPasswordField(20);
-		passwordText.setBounds(115, 200, 150, 25);
+		passwordText.setBounds(450, 320, 150, 25);
 		panel.add(passwordText);
 		//
 		
+		CreateAccount = new JButton("Create Account");
+		CreateAccount.setBounds(350, 380, 150, 20);
+		CreateAccount.addActionListener(new GUI());
+		panel.add(CreateAccount);
+		//
 		LoginButton = new JButton("Login");
-		LoginButton.setBounds(240, 240, 70, 20);
+		LoginButton.setBounds(560, 380, 120, 20);
 		LoginButton.addActionListener(new GUI());
 		panel.add(LoginButton);
 		//
