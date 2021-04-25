5066139f9f5dceefb358b118a68fe173cc29685e
gkaplan21 <82334723+gkaplan21@users.noreply.github.com>
2021-04-24 23:28:29 -0700

    Add files via upload

---
+++ DB/Account.java
@@ -0,0 +1,66 @@
+package DB;
+
+public class Account {
+	private int type;
+	private String name;
+	private String username;
+	private String password;
+	private String email;
+	private String address;
+	
+	public Account (int type, String name, String username, String password, String email, String address) {
+		this.type = type;
+		this.name = name;
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.address = address;
+	}
+	
+	public Account() {
+		username = null;
+	}
+	
+	public void print() {
+		System.out.print("Type: ");
+		switch(type) {
+		case 1: System.out.println("Customer");
+				break;
+		case 2: System.out.println("Clerk");
+				break;
+		case 3: System.out.println("Manager");
+				break;
+		default: System.out.println("Unknown account type");
+				break;
+		}
+		System.out.println("Name: " + name);
+		System.out.println("Username: " + username);
+		System.out.println("Password: " + password);
+		System.out.println("Email: " + email);
+		System.out.println("Address: " + address + "\n");
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+}
---
+++ DB/Item.java
@@ -0,0 +1,42 @@
+package DB;
+
+public class Item {
+	private String title;
+	private float price;
+	private String description;
+	private int stock;
+	
+	public Item (String title, float price, String description, int stock) {
+		this.title = title;
+		this.price = price;
+		this.description = description;
+		this.stock = stock;
+	}
+	
+	public Item () {
+		
+	}
+	
+	public void print() {
+		System.out.println("Title: " + title);
+		System.out.println("Price: " + price);
+		System.out.println("Description: " + description);
+		System.out.println("Stock: " + stock);
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public float getPrice() {
+		return price;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public int getStock() {
+		return stock;
+	}
+}
