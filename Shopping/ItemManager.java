5aa16a09be853a45dafd6e7256272eeeb58c0174
rwillar <82558065+rwillar@users.noreply.github.com>
2021-04-25 17:51:40 -0700

    Add files via upload

--- Shopping/ItemManager.java
+++ Shopping/ItemManager.java
@@ -1,6 +1,6 @@
 package Shopping;
-
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -10,54 +10,100 @@ import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import Accounts.Login;
-import DB.PostgresAccounts;
+import DB.Item;
+import Shopping.ItemManager;
+import Shopping.PendingOrders;
+import Shopping.ShoppingPage;
+import DB.PostgresInventory;
 
 public class ItemManager implements ActionListener {
-	private JPanel panel = new JPanel();
-	private JButton logout;
+	private JFrame frame;
+	private JPanel panel;
+	private JTextField searchBar, quantity, price;
+	private JButton logout, newItem, search, delete, confirm;
 	private JComboBox choice;
+
+
+	 Item bread = new Item("Bread", 2.00f, "Made of wheat", 3);
+	 Item milk = new Item("Milk", 3.00f, "Cow go moo", 5);
+	 Item cereal = new Item("Cereal", 4.00f, "Grains", 7);
+	private String itemSearch;
+	private String item;
 	
-	public ItemManager() {
-		panel;
+	
+
+	public void ItemManager() { 
+		System.out.println();
+		
+
+		panel= new JPanel();
+		frame= new JFrame();
+		frame.setSize(1200,850);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(panel);
+		
+		
+		
 
 		JLabel title = new JLabel("Item Manager");
 		title.setBounds(425, 10, 1200, 100);
 		panel.add(title);
 		title.setFont(new Font(null, Font.BOLD,55));
-		
-		ArrayList<String> optionsAL = new ArrayList<>();
-		//if(PostgresAccounts.getAccount("user").getType() == 2) {
-		//	optionsAL.add("Pending Orders");
-		//	optionsAL.add("Item Manager");
-		//} else if (PostgresAccounts.getAccount("user").getType() == 3) {
-		//	optionsAL.add("Pending Orders");
-		//	optionsAL.add("Item Manager");
-		//}
-		optionsAL.add("Shopping Page");
-		optionsAL.add("Pending Orders");
-		optionsAL.add("Item Manager");
-		
-		String[] options = new String[optionsAL.size()];
-		for(int i = 0; i < optionsAL.size(); i++) {
-			options[i] = optionsAL.get(i);
+
+		frame.setVisible(true);
+
+	}
+	public  void find(String search) {
+
+		//will be the ArrayList with all items
+		ArrayList<Item> itemList = new ArrayList<Item>();
+
+		itemList.add(bread);
+		itemList.add(milk);
+		itemList.add(cereal);
+
+		for(int i = 0; i < itemList.size(); i++) {
+
+
+			Item itemSearch = itemList.get(i);
+			if(search == itemSearch.getTitle()) {
+				//itemSearch.print();
+				String item = itemSearch.getTitle();
+				System.out.println(item);
+				//trying to make this search query have addItem and deleteItem options.
+			}
+
 		}
-		
-		choice = new JComboBox(options);
-		choice.setSelectedIndex(2);
-		panel.add(choice);
-		choice.setBounds(100, 50, 150, 25);
-		choice.addActionListener(this);
-		panel.setLayout(null);
-		
-		logout = new JButton("Log Out ->");
-		panel.add(logout);
-		logout.setBounds(950, 50, 150, 25);
-		logout.addActionListener(this);
+
+
+	}
+
+	public void deleteItem(String itemSearch) {
+		this.item = itemSearch;
+		System.out.println("Delete " + itemSearch + "?"); //make this into GUI 
+		//when JButton confirm, then 
+		deleteItem(itemSearch);
+	}
+
+	public void addItem(String itemSearch) {
+		this.item = itemSearch;
+		addItem(itemSearch);
+	}
+	public void addPanel(JFrame frame) {
+		 frame.add(panel);
 	}
 	
+	public void removePanel(JFrame frame) {
+		frame.remove(panel);
+	}
+	
+
+	@Override
 	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
 		if(e.getSource() == choice) {
 			int i = choice.getSelectedIndex();
 			if(i == 0) {
@@ -69,9 +115,10 @@ public class ItemManager implements ActionListener {
 			}
 		}
 		if(e.getSource() == logout) { // Log out the user
+			Driver.newChoice(0);
 
-			Login.main(new String[1]); // TODO: Fix!
-		}
-		
 	}
+		//if(e.getSource == )
+
+}
 }
--- Shopping/PendingOrders.java
+++ Shopping/PendingOrders.java
@@ -1,6 +1,7 @@
 package Shopping;
 
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -15,11 +16,23 @@ import Accounts.Login;
 import DB.PostgresAccounts;
 
 public class PendingOrders extends ShoppingCart implements ActionListener {
-	private JPanel panel = new JPanel();
+	private JFrame frame;
+	private JPanel panel;
 	private JButton logout;
-	private JComboBox choice;
+	private JComboBox choice, items;
 	
+	public void displayPanel(JFrame frame) {
+	     frame.add(panel);
+	}
 	public PendingOrders() {
+		
+	
+		panel= new JPanel();
+		frame= new JFrame();
+		frame.setSize(1200,850);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(panel);
+
 		JLabel title = new JLabel("Pending Orders");
 		title.setBounds(400, 10, 1200, 100);
 		panel.add(title);
@@ -27,12 +40,12 @@ public class PendingOrders extends ShoppingCart implements ActionListener {
 		
 		ArrayList<String> optionsAL = new ArrayList<>();
 		//if(PostgresAccounts.getAccount("user").getType() == 2) {
-		//	optionsAL.add("Pending Orders");
-		//	optionsAL.add("Item Manager");
-		//} else if (PostgresAccounts.getAccount("user").getType() == 3) {
-		//	optionsAL.add("Pending Orders");
-		//	optionsAL.add("Item Manager");
-		//}
+				//	optionsAL.add("Pending Orders");
+				//	optionsAL.add("Item Manager");
+				//} else if (PostgresAccounts.getAccount("user").getType() == 3) {
+				//	optionsAL.add("Pending Orders");
+				//	optionsAL.add("Item Manager");
+				//}
 		optionsAL.add("Shopping Cart");
 		optionsAL.add("Pending Orders");
 		optionsAL.add("Item Manager");
@@ -41,6 +54,22 @@ public class PendingOrders extends ShoppingCart implements ActionListener {
 		for(int i = 0; i < optionsAL.size(); i++) {
 			options[i] = optionsAL.get(i);
 		}
+		
+		JLabel order1 = new JLabel("Order 1");
+		JLabel order2 = new JLabel("Order 2");
+		JLabel order3 = new JLabel("Order 3");
+		JLabel order4 = new JLabel("Order 4");
+		JLabel order5 = new JLabel("Order 5");
+		JLabel order6 = new JLabel("Order 6");
+		
+		panel.setLayout(new GridLayout(2,6));
+		panel.add(order1);
+		panel.add(order2);
+		panel.add(order3);
+		panel.add(order4);
+		panel.add(order5);
+		panel.add(order6);
+		
 		
 		choice = new JComboBox(options);
 		choice.setSelectedIndex(1);
@@ -53,23 +82,61 @@ public class PendingOrders extends ShoppingCart implements ActionListener {
 		panel.add(logout);
 		logout.setBounds(950, 50, 150, 25);
 		logout.addActionListener(this);
+		
+		frame.setVisible(true);
+		
+		
 	}
+	public void addPanel(JFrame frame) {
+		 frame.add(panel);
+	}
+	
+	public void removePanel(JFrame frame) {
+		frame.remove(panel);
+	}
+	
+
+	
+	
 	
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource() == choice) {
 			int i = choice.getSelectedIndex();
 			if(i == 0) {
+				//frame.dispose();
+				
+				
+				/*
+				newChoice =
+				0 - Login
+				1 - Create Account Customer
+				2 - Create Account Employee
+				3 - Shopping
+				4 - Item Manager
+				5 - Pending Orders
+				6 - Logout (leads back to Login page)
+				Driver.getUsername();
+				Driver.setUsername();
+				 
+				 */
 				Driver.newChoice(3);
+				
 			} else if(i == 1) {
+				
 				Driver.newChoice(5);
+				
 			} else if (i == 2) {
+			
 				Driver.newChoice(4);
+				
 			}
 		}
 		if(e.getSource() == logout) { // Log out the user
-
-			Login.main(new String[1]); // TODO: Fix!
+			
+			Driver.newChoice(6);
+			
 		}
 		
 	}
+	
 }
