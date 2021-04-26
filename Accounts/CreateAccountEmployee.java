65a098854338dfb0419fe30848944e33f1e81f31
gkaplan21 <82334723+gkaplan21@users.noreply.github.com>
2021-04-25 18:11:40 -0700

    Add files via upload

---
+++ Accounts/CreateAccountEmployee.java
@@ -0,0 +1,175 @@
+package Accounts;
+import DB.*;
+import Shopping.ShoppingPage;
+import main.Driver;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+
+public class CreateAccountEmployee implements ActionListener{
+	//private static PostgresAccounts pAccount = new PostgresAccounts();
+	private JLabel UseNameLabel;
+	private JTextField UserNameText;
+	private JLabel PasswordLabel;
+	private JPasswordField PasswordText;
+	private JLabel FirstName;
+	private JTextField FirstNameText;
+	private JLabel LastName;
+	private JTextField LastNameText;
+	private JLabel Email;
+	private JTextField EmailText;
+	private JComboBox choice;
+	private JLabel position;
+	private JComboBox positionChoice;
+	private JButton CreateAccount;
+	private int i=1;
+	private JPanel panel;	
+	private JComboBox navigation;
+	private int ii=1;
+		
+	public CreateAccountEmployee(){			
+		panel= new JPanel();
+		panel.setLayout(null);
+
+		JLabel title = new JLabel(" Create Account ");
+		title.setBounds(220, 150, 1200, 100);
+		panel.add(title);
+		title.setFont(new Font( null, Font.BOLD,55));	
+		
+		String [] options= {"Customer","Employee"};
+		choice = new JComboBox(options);
+		choice.setSelectedIndex(1);
+		panel.add(choice);
+		choice.setBounds(800, 400, 300, 25);
+		choice.addActionListener(this);
+		
+		
+				
+				//
+				
+		UseNameLabel = new JLabel(" User Name:");
+		UseNameLabel.setBounds(200, 300, 200, 25);
+		UseNameLabel.setFont(new Font( null, Font.BOLD,20));
+		panel.add(UseNameLabel);
+				
+		UserNameText = new JTextField(20);
+		UserNameText.setBounds(350, 300, 250, 25);
+		panel.add(UserNameText);
+		
+		
+				//
+		PasswordLabel = new JLabel(" Password: ");
+		PasswordLabel.setBounds(200, 350, 200, 25);
+		PasswordLabel.setFont(new Font( null, Font.BOLD,20));
+		panel.add(PasswordLabel);
+				
+		PasswordText = new JPasswordField(20);
+		PasswordText.setBounds(350, 350, 250, 25);
+		panel.add(PasswordText);
+				//
+				
+		FirstName = new JLabel(" First Name:");
+		FirstName.setBounds(200, 400, 250, 25);
+		FirstName.setFont(new Font( null, Font.BOLD,20));
+		panel.add(FirstName);
+				
+		FirstNameText = new JTextField(20);
+		FirstNameText.setBounds(350, 400, 250, 25);
+		panel.add(FirstNameText);
+				//
+		LastName = new JLabel(" Last Name:");
+		LastName.setBounds(200, 450, 250, 25);
+		LastName.setFont(new Font( null, Font.BOLD,20));
+		panel.add(LastName);
+				
+		LastNameText = new JTextField(20);
+		LastNameText.setBounds(350, 450, 250, 25);
+		panel.add(LastNameText);
+				//
+		Email = new JLabel(" Email: ");
+		Email.setBounds(200, 500, 250, 25);
+		Email.setFont(new Font( null, Font.BOLD,20));
+		panel.add(Email);
+				
+		EmailText = new JTextField(20);
+		EmailText.setBounds(350, 500, 250, 25);
+		panel.add(EmailText);
+				//
+		position = new JLabel(" Position: ");
+		position.setBounds(200, 550, 250, 25);
+		position.setFont(new Font( null, Font.BOLD,20));
+		panel.add(position);
+				//
+		
+		String [] position= {"employee","manager"};
+		positionChoice = new JComboBox(position);
+		panel.add(positionChoice);
+		positionChoice.setBounds(350, 550, 250, 25);
+		panel.setLayout(null);
+				//
+		CreateAccount = new JButton("CreateAccount");
+		CreateAccount.setBounds(650, 600, 160, 40);
+		CreateAccount.addActionListener(this);
+		panel.add(CreateAccount);
+				//
+		String [] navigations= {"Login Page","Create Account Page"};
+		navigation = new JComboBox(navigations);
+		navigation.setSelectedIndex(1);
+		panel.add(navigation);
+		navigation.setBounds(10, 10, 150, 25);
+		navigation.addActionListener(this);
+		}
+	
+	
+public void addPanel(JFrame frame) {
+		frame.add(panel);
+		
+	}
+	
+public void removePanel(JFrame frame) {
+		frame.remove(panel);
+		
+		
+	}
+		
+	  public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+		if(e.getSource()==choice) {
+			i=choice.getSelectedIndex();
+
+		if(i==0) {
+			Driver.newChoice(1);
+				}
+			}
+		
+	if(e.getSource()==CreateAccount) {
+				//frame.dispose();??????????????????????????
+			}
+	 ii=navigation.getSelectedIndex();
+ 	switch (ii){
+		 case 0:
+			Driver.newChoice(0);
+			break;
+			 
+		 case 1:
+			 
+			 break;
+		 
+		 case 2:
+			 Driver.newChoice(3);
+			 break;
+		 }
+		
+	  }
+}
+
---
+++ Accounts/Login.java
@@ -0,0 +1,137 @@
+package Accounts;
+import DB.*;
+import main.Driver;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+
+
+public class Login implements ActionListener {
+	private static JLabel useNameLabel;
+	private static JTextField userNameText;
+	private static JLabel passwordLabel;
+	private static JPasswordField passwordText;
+	private static JButton LoginButton;
+	private static JButton CreateAccount;
+	private static JLabel LoginSuccessful;
+	private static JPanel panel;	
+	private static PostgresAccounts pAccount = new PostgresAccounts();	 
+	
+
+	public Login() {
+		
+		ImageIcon image= new ImageIcon
+				("C:\\Users\\Sal\\eclipse-workspace\\GUI\\src\\Accounts\\ss.png"); //setting an image 
+		//Change and copy the image path on C
+		
+	    panel= new JPanel();
+	 
+		JLabel title = new JLabel(" Clothing Shop ");
+		title.setBounds(220, 150, 1200, 100);
+		panel.add(title);
+		title.setFont(new Font( null, Font.BOLD,60));
+		title.setForeground(Color.white);
+
+		panel.setLayout(null);
+	
+		//
+		
+		useNameLabel = new JLabel(" User Name:");
+		useNameLabel.setBounds(200, 300, 200, 25);
+		useNameLabel.setFont(new Font( "null", Font.BOLD,20));
+		useNameLabel.setForeground(Color.white);
+		panel.add(useNameLabel);
+		
+		userNameText = new JTextField(20);
+		userNameText.setBounds(350, 300, 250, 25);
+		panel.add(userNameText);
+		//
+		
+		passwordLabel = new JLabel(" Password:");
+		passwordLabel.setBounds(200, 400, 200, 25);
+		passwordLabel.setFont(new Font( "null", Font.BOLD,20));
+		passwordLabel.setForeground(Color.white);
+		panel.add(passwordLabel);
+		
+		passwordText = new JPasswordField(20);
+		passwordText.setBounds(350, 400, 250, 25);
+		panel.add(passwordText);
+		//
+		
+		CreateAccount = new JButton("Create Account");
+		CreateAccount.setBounds(250, 480, 160, 20);
+		panel.add(CreateAccount);
+		CreateAccount.addActionListener(this);
+		//
+		LoginButton = new JButton("Login");
+		LoginButton.setBounds(500, 480, 160, 20);
+		LoginButton.addActionListener(this);
+		panel.add(LoginButton);
+		//
+		
+		LoginSuccessful = new JLabel("");
+		LoginSuccessful.setBounds(650, 340, 300, 20);
+		panel.add(LoginSuccessful);
+		//
+	
+		JLabel img= new JLabel();
+		img.setIcon(image);
+		panel.add(img);
+		img.setBounds(0, 0, 1200, 850);
+		
+		JLabel backgrounnd = new JLabel(); 
+		backgrounnd.setOpaque(true);
+		backgrounnd.setBackground(Color.DARK_GRAY);
+		backgrounnd.setBounds (0, 0, 1200, 850);
+        panel.add(backgrounnd);
+		
+	}
+	
+	public void addPanel(JFrame frame) {
+		frame.add(panel);
+	}
+	
+	public void removePanel(JFrame frame) {
+		frame.remove(panel);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	
+	
+	if(e.getSource()==CreateAccount) {
+		Driver.newChoice(1);
+	}
+	
+	String user = userNameText.getText();
+	@SuppressWarnings("deprecation")
+	String password = passwordText.getText();
+	
+	
+	if (e.getSource()== LoginButton) {
+		if (pAccount.accountExists(user) && pAccount.getAccount(user).getPassword().equals(password)) {
+		JOptionPane.showMessageDialog(null, "Login Successful", "Login", JOptionPane.PLAIN_MESSAGE );
+			Driver.newChoice(3);
+			
+	} else {
+		JOptionPane.showMessageDialog(null, "Wrong user name or password", "Login", JOptionPane.ERROR_MESSAGE);
+		}
+	
+	}
+  }
+}
+	
+
+
